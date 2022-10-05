4fce0a3240d0bbb6dec54c9dacc8a6e736967f80
victorvenk <victorolamy@gmail.com>
2022-10-05 17:15:22 +0100

    admission status page

--- src/pages/admission/Admission.jsx
+++ src/pages/admission/Admission.jsx
@@ -1,7 +1,23 @@
-import "./admission.css"
+import "./admission.css";
+import { Search } from "@mui/icons-material";
 
 export default function Admission() {
   return (
-	<div className="admission">Admission</div>
+	<div className="admission">
+    <div className="admissionWrapper">
+      <div className="admissionContainer">
+        <div className="admissionTitle">
+          <h2>Your Profile</h2>
+          <p><b>Hi Victor</b> Welcome to your Admission Status Page where you verify your current admission status<br></br> as applied on the school portal</p>
+          <button className="admissionBtn">Confirm Status</button>
+        </div>
+        <div className="admissionImg">
+          <img src="./src/assets/vec.jpg" alt="vectorimg" className="admissionVec"/>
+        </div>
+      </div>
+    </div>
+
+    
+  </div>
   )
 }
